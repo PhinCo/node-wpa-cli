059fe39a81d02234edac689e4a7a21b0d3f6131e
Scott <scott@phin.co>
2018-09-25 17:32:44 -0700

    convert to ES6 class

--- src/index.js
+++ src/index.js
@@ -1,248 +1,248 @@
-var unix = require('unix-dgram'),
-    inherits = require('util').inherits,
-    EventEmitter = require('events').EventEmitter;
+const unix = require('unix-dgram')
+const EventEmitter = require('events').EventEmitter
 
-inherits(WpaCLI, EventEmitter);
+class APStation {
+	constructor(bssid, frequency, signal, encryption, ssid) {
+		const encRegex = /\[([A-Z0-9\-]+)\]/g
 
-function WpaCLI(ifname) {
-    if (!(this instanceof WpaCLI)) return new WpaCLI(ifname);
+		const encArray = [];
+		let match;
 
-    if (typeof ifname !== 'string') {
-        throw new Error('ifname should be a string')
-    }
+		while ((match = encRegex.exec(encryption)) !== null)
+			encArray.push(match[1]);
 
-    this.ignoreAck = false;
-
-    EventEmitter.call(this);
-    this.ifname = ifname
+		this.bssid = bssid
+		this.frequency = frequency
+		this.encryption = encArray
+		this.signal = signal
+		this.ssid = ssid
+	}
 }
 
-WpaCLI.prototype.connect = function (callback) {
-    var serverPath = '/var/run/wpa_supplicant/' + this.ifname;
-    var clientPath = '/tmp/wpa_ctrl' + Math.random().toString(36).substr(1);
-    var error = this._onError.bind(this);
+class WpaCLI extends EventEmitter {
+	constructor(ifname) {
+		super()
 
-    this.client = unix.createSocket('unix_dgram')
-        .on('message', this._onMessage.bind(this))
-        .on('error', error);
+		if (typeof ifname !== 'string') {
+			throw new Error('ifname must be a string')
+		}
 
-    // I should probably rewrite this using promises..
-    this._connect(serverPath, function (err) {
-        if (err) return error('unable to connect to interface');
-        this.listen(clientPath, function (err) {
-            if (err) return error('unable to listen for events');
-            this.attach(function (err) {
-                if (err) return error('unable to attach to events');
+		this.ignoreAck = false
+		this.ifname = ifname
+	}
 
-                this.emit('connect');
-                if (typeof callback === 'function')
-                    callback();
-            });
-        });
-    });
-};
+	async connect(callback) {
+		const self = this
 
-WpaCLI.prototype._onError = function (err) {
-    if (this._handleError)
-        this._handleError(err);
-    else
-        this.emit('error', err);
-};
+			const serverPath = '/var/run/wpa_supplicant/' + this.ifname;
+			const clientPath = '/tmp/wpa_ctrl' + Math.random().toString(36).substr(1);
+			const error = this._onError.bind(this);
 
-WpaCLI.prototype._connect = function (path, cb) {
-    var done = function (err) {
-        this.client.removeListener('connect', done);
-        delete this._handleError;
-        cb.call(this, err)
-    }.bind(this);
+			this.client = unix.createSocket('unix_dgram')
+				.on('message', this._onMessage.bind(this))
+				.on('error', error);
 
-    this._handleError = done;
-    this.client.once('connect', done)
-        .connect(path)
-};
+			// I should probably rewrite this using promises..
+			this._connect(serverPath, function (error) {
+				if (error) return error('unable to connect to interface');
+				self.listen(clientPath, function (error) {
+					if (error) return error('unable to listen for events');
+					self.attach(function (error) {
+						if (error) return error('unable to attach to events');
 
-WpaCLI.prototype.listen = function (clientPath, cb) {
-    var done = function (err) {
-        this.client.removeListener('listening', done);
-        delete this._handleError;
-        cb.call(this, err)
-    }.bind(this);
+						self.emit('connect');
+						if (typeof callback === 'function')
+							callback();
+					})
+				})
+			})
+	}
 
-    this._handleError = done;
-    this.client.once('listening', done)
-        .bind(clientPath)
-};
+	_onError(error) {
+		if (this._handleError) return this._handleError(error);
+		this.emit('error', error);
+	}
 
-WpaCLI.prototype.request = function (req, cb) {
-    this._handleReply = cb;
-    this.client.send(new Buffer(req));
-};
+	_connect(path, cb) {
+		const done = function (error) {
+			this.client.removeListener('connect', done);
+			delete this._handleError;
+			cb.call(this, error)
+		}.bind(this);
 
-WpaCLI.prototype._onMessage = function (msg) {
-    var handleReply;
-    this.emit('rawMsg', msg);
+		this._handleError = done;
+		this.client.once('connect', done)
+			.connect(path)
+	}
 
-    if (msg.length > 3 && msg[0] === 60 && msg[2] === 62) {
-        this._onCtrlEvent(msg[1] - 48, msg.slice(3))
-    } else if (this.ignoreAck && msg.toString().substr(0, 3).indexOf('OK') != -1) { // This is just an ack message, ignoring it...
-        this.ignoreAck = false;
-    } else if ((handleReply = this._handleReply)) {
-        delete this._handleReply;
-        handleReply.call(this, msg.toString().trim())
-    }
-};
+	listen(clientPath, cb) {
+		const done = function (error) {
+			this.client.removeListener('listening', done);
+			delete this._handleError;
+			cb.call(this, error)
+		}.bind(this);
 
-WpaCLI.prototype._onCtrlEvent = function (level, msg) {
-    var messageParts = msg.toString().split(' ');
+		this._handleError = done;
+		this.client.once('listening', done)
+			.bind(clientPath)
+	}
 
-    var messageName = messageParts[0];
-    messageParts.splice(1);
+	request(req, cb) {
+		this._handleReply = cb;
+		this.client.send(new Buffer(req));
+	}
 
-    this.emit(messageParts);
-    this.emit('event-' + level, messageName, messageParts);
-};
+	_onMessage(msg) {
+		let handleReply;
+		this.emit('rawMsg', msg);
 
-WpaCLI.prototype.setLevel = function (level, cb) {
-    this.request('LEVEL ' + level, function (msg) {
-        if (msg === 'OK')
-            cb.call(this, null);
-        else
-            cb.call(this, 'level: ' + msg);
-    });
-};
+		if (msg.length > 3 && msg[0] === 60 && msg[2] === 62) {
+			this._onCtrlEvent(msg[1] - 48, msg.slice(3))
+		} else if (this.ignoreAck && msg.toString().substr(0, 3).indexOf('OK') !== -1) { // This is just an ack message, ignoring it...
+			this.ignoreAck = false;
+		} else if ((handleReply = this._handleReply)) {
+			delete this._handleReply;
+			handleReply.call(this, msg.toString().trim())
+		}
+	}
 
-WpaCLI.prototype.attach = function (cb) {
-    this.request('ATTACH', function (msg) {
-        if (msg === 'OK')
-            cb.call(this, null);
-        else
-            cb.call(this, 'attach: ' + msg);
-    });
-};
+	_onCtrlEvent(level, msg) {
+		const messageParts = msg.toString().split(' ');
 
-WpaCLI.prototype.detach = function (cb) {
-    this.request('DETACH', function (msg) {
-        if (msg === 'OK')
-            cb.call(this, null);
-        else
-            cb.call(this, 'detach: ' + msg);
-    })
-};
+		const messageName = messageParts[0];
+		messageParts.splice(1);
 
-WpaCLI.prototype.getStatus = function (cb) {
-    this.ignoreAck = true;
-    this.request('STATUS', function (msg) {
-        var status = {};
-        var lines = msg.toString().split('\n');
-        for (var i = 0; i < lines.length; i++) {
-            var line = lines[i];
-            var j = line.indexOf('=');
-            if (j > 0) {
-                status[line.substr(0, j)] = line.substr(j + 1)
-            }
-        }
+		this.emit(messageParts);
+		this.emit('event-' + level, messageName, messageParts);
+	}
 
-        if (status.wpa_state)
-            cb.call(this, null, status);
-        else
-            cb.call(this, 'unable to get status');
-    });
-};
+	setLevel(level, cb) {
+		this.request('LEVEL ' + level, function (msg) {
+			if (msg === 'OK')
+				cb.call(this, null);
+			else
+				cb.call(this, 'level: ' + msg);
+		});
+	}
 
-function APStation(bssid, frequency, signal, encryption, ssid) {
-    var encRegex = /\[([A-Z0-9\-]+)\]/g;
+	attach(cb) {
+		this.request('ATTACH', function (msg) {
+			if (msg === 'OK')
+				cb.call(this, null);
+			else
+				cb.call(this, 'attach: ' + msg);
+		});
+	}
 
-    var encArray = [];
-    var match;
+	detach(cb) {
+		this.request('DETACH', function (msg) {
+			if (msg === 'OK')
+				cb.call(this, null);
+			else
+				cb.call(this, 'detach: ' + msg);
+		})
+	}
 
-    while ((match = encRegex.exec(encryption)) != null)
-        encArray.push(match[1]);
+	getStatus(cb) {
+		this.ignoreAck = true;
+		this.request('STATUS', function (msg) {
+			const status = {};
+			const lines = msg.toString().split('\n');
+			for (let i = 0; i < lines.length; i++) {
+				const line = lines[i];
+				const j = line.indexOf('=');
+				if (j > 0) {
+					status[line.substr(0, j)] = line.substr(j + 1)
+				}
+			}
 
-    return {
-        bssid: bssid,
-        frequency: frequency,
-        encryption: encArray,
-        signal: signal,
-        ssid: ssid
-    };
+			if (status.wpa_state)
+				cb.call(this, null, status);
+			else
+				cb.call(this, 'unable to get status');
+		});
+	}
+
+	getScanResults(cb) {
+		this.ignoreAck = true;
+		this.request('SCAN_RESULTS', function (msg) {
+			const stations = [];
+			const lines = msg.toString().split('\n');
+
+			for (let i = 1; i < lines.length; i++) {
+				const lineSplit = lines[i].split('\t');
+				stations.push(new APStation(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], lineSplit[4]));
+			}
+
+			cb.call(this, null, stations);
+		});
+	}
+
+	scan(cb) {
+		this.ignoreAck = true;
+		this.request('SCAN');
+
+		this.once('CTRL-EVENT-SCAN-RESULTS', cb);
+	}
+
+	addNetwork(params, cb) {
+		const self = this
+
+		if (typeof params !== 'object') {
+			cb.call(this, 'wrong params type');
+			return false;
+		}
+
+		this.ignoreAck = true;
+		let done = false;
+
+		this.request('ADD_NETWORK', function (network_id) {
+			for (let key in params)
+				if (done) {
+					break
+				} else if (params.hasOwnProperty(key)) {
+					const request = 'SET_NETWORK ' + network_id + ' ' + key + ' ' + params[key]
+					console.log("REQUEST: ", request)
+					self.request(request, function (status) {
+						if (status !== 'OK') {
+							if (typeof cb === 'function')
+								cb.call(this, 'Param error')
+							done = true;
+						}
+
+					})
+				}
+
+			if (!done && typeof cb === 'function')
+				cb.call(this, null, network_id)
+		})
+	}
+
+	removeAllNetworks(cb) {
+		this.request('REMOVE_NETWORK ' + 'all', cb)
+	}
+
+	removeNetwork(netId, cb) {
+		this.request('REMOVE_NETWORK ' + netId, cb)
+	}
+
+	disableAllNetworks(netId, cb) {
+		this.request('DISABLE_NETWORK ' + 'all', cb)
+	};
+
+	disableNetwork(netId, cb) {
+		this.request('DISABLE_NETWORK ' + netId, cb)
+	};
+
+	enableNetwork(netId, cb) {
+		this.request('ENABLE_NETWORK ' + netId, cb)
+	};
+
+	save(cb) {
+		this.request('SAVE_CONFIG', cb)
+	};
+
 }
-
-WpaCLI.prototype.getScanResults = function (cb) {
-    this.ignoreAck = true;
-    this.request('SCAN_RESULTS', function (msg) {
-        var stations = [];
-        var lines = msg.toString().split('\n');
-
-        for (var i = 1; i < lines.length; i++) {
-            var lineSplit = lines[i].split('\t');
-            stations.push(new APStation(lineSplit[0], lineSplit[1], lineSplit[2], lineSplit[3], lineSplit[4]));
-        }
-
-        cb.call(this, null, stations);
-    });
-};
-
-WpaCLI.prototype.scan = function (cb) {
-    this.ignoreAck = true;
-    this.request('SCAN');
-
-    this.once('CTRL-EVENT-SCAN-RESULTS', cb);
-};
-
-WpaCLI.prototype.addNetwork = function (params, cb) {
-    if (typeof params !== 'object') {
-        cb.call(this, 'wrong params type');
-        return false;
-    }
-
-    this.ignoreAck = true;
-    var done = false;
-
-    this.request('ADD_NETWORK', function (network_id) {
-        for (var key in params)
-            if (done) {
-                break;
-            } else if (params.hasOwnProperty(key)) {
-            	const request = 'SET_NETWORK ' + network_id + ' ' + key + ' ' + params[key];
-            	console.log( "REQUEST: ", request );
-                this.request( request, function (status) {
-                    if (status != 'OK') {
-                        if (typeof cb === 'function')
-                            cb.call(this, 'Param error');
-                        done = true;
-                    }
-
-                });
-            }
-
-        if (!done && typeof cb === 'function')
-            cb.call(this, null, network_id);
-    });
-};
-
-WpaCLI.prototype.removeAllNetworks = function (cb) {
-	this.request('REMOVE_NETWORK ' + 'all', cb);
-};
-
-WpaCLI.prototype.removeNetwork = function (netId, cb) {
-    this.request('REMOVE_NETWORK ' + netId, cb);
-};
-
-WpaCLI.prototype.disableAllNetworks = function (netId, cb) {
-	this.request('DISABLE_NETWORK ' + 'all', cb);
-};
-
-WpaCLI.prototype.disableNetwork = function (netId, cb) {
-    this.request('DISABLE_NETWORK ' + netId, cb);
-};
-
-WpaCLI.prototype.enableNetwork = function (netId, cb) {
-    this.request('ENABLE_NETWORK ' + netId, cb);
-};
-
-WpaCLI.prototype.save = function( cb ){
-    this.request('SAVE_CONFIG', cb );
-};
 
 module.exports = WpaCLI;
 
